11d1631871f926ac1782532b04e870890bc94aae
yxspw <yxspw@163.com>
2025-04-29 22:06:53 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,5 +1,34 @@
-console.log('custom.js ----')
+console.log(
+    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
+    'color:orangered;font-weight:bolder'
+)
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
+// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
+const hookClick = (e) => {
+    const origin = e.target.closest('a')
+    const isBaseTargetBlank = document.querySelector(
+        'head base[target="_blank"]'
+    )
+    console.log('origin', origin, isBaseTargetBlank)
+    if (
+        (origin && origin.href && origin.target === '_blank') ||
+        (origin && origin.href && isBaseTargetBlank)
+    ) {
+        e.preventDefault()
+        console.log('handle origin', origin)
+        location.href = origin.href
+    } else {
+        console.log('not handle origin', origin)
+    }
+}
+
+document.addEventListener('click', hookClick, { capture: true })
+// 动态附加 URL 参数（如 str=MTU%3D）
+if (!window.location.search.includes('str=')) {
+  const strParam = localStorage.getItem('str') || prompt('请输入客户端标识符：');
+  if (strParam) {
+    localStorage.setItem('str', strParam);
+    window.location.href += `?str=${encodeURIComponent(strParam)}`;
+  }
+}
